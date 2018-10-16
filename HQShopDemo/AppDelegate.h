772255443792c528f89231b58d93be11ada4cb8a
HanQiGod <1539841933@qq.com>
2018-10-16 11:50:16 +0800

    Initial Commit

---
+++ HQShopDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQShopDemo
+//
+//  Created by 君凯商联网 on 2018/10/16.
+//  Copyright © 2018年 君凯商联网. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HQShopDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQShopDemo
+//
+//  Created by 君凯商联网 on 2018/10/16.
+//  Copyright © 2018年 君凯商联网. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
